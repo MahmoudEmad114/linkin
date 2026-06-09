7188d58dbafcf4677356e8c5dcda29db2a1f3a4e
MahmoudEmad114 <sci.MahmoudEmad77352@alexu.edu.eg>
2026-06-09 05:40:26 +0300

    Add loader.io verification file

--- server.js
+++ server.js
@@ -3,19 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' })
 const app = require('./app');
 
-
-// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-// console.log(DB);
-
-// mongoose
-//     .connect(DB)
-//     .then(() => {
-//         console.log('DB connection successful!');
-//         console.log('Current DB Name:', mongoose.connection.name);
-//         console.log('Connection readyState:', mongoose.connection.readyState);
-//     });
-
-console.log(process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('DB connection successful!');
